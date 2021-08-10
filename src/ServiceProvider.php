2f8dd64b6be6ac198ef75b834954efa4c55f099e
Dave Lemmens <davesweb@gmail.com>
2021-08-10 21:03:39 +0200

    Add bindings for Bricklink API

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace Davesweb\LaravelBricklinkApi;
 
+use Davesweb\BrinklinkApi\Bricklink;
+use Davesweb\BrinklinkApi\BricklinkConfig;
+use Illuminate\Contracts\Config\Repository;
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\ServiceProvider as IlluminateServiceProvider;
 
 class ServiceProvider extends IlluminateServiceProvider
@@ -15,7 +19,20 @@ class ServiceProvider extends IlluminateServiceProvider
     
     public function register()
     {
-        // todo once the other package is public
-        //$this->app->bind();
+        $this->app->bind(BricklinkConfig::class, function(Application $app) {
+            /** @var Repository $config */
+            $config = $app->make('config');
+            
+            return new BricklinkConfig(
+                $config->get('bricklink.consumer_key'),
+                $config->get('bricklink.consumer_secret'),
+                $config->get('bricklink.token_value'),
+                $config->get('bricklink.token_secret'),
+            );
+        });
+    
+        $this->app->bind(Bricklink::class, function(Application $app) {
+            return new Bricklink($app->make(BricklinkConfig::class), null, null);
+        });
     }
 }
